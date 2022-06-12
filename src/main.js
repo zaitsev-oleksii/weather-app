94079f76f5f59bc27af04d610a265cb35e4d9a3d
Oleksii <zaytsev961st@gmail.com>
2022-06-12 04:01:14 +0300

    saving loc/prefs to localstorage

--- src/main.js
+++ src/main.js
@@ -11,4 +11,28 @@ const app = createApp(App);
 app.use(router);
 app.use(store);
 
+const locFromLS = window.localStorage.getItem("weather-location");
+if (locFromLS) {
+  store.commit("setLocation", JSON.parse(locFromLS));
+}
+const prefsFromLS = window.localStorage.getItem("weather-preferences");
+if (prefsFromLS) {
+  store.commit("setPreferences", JSON.parse(prefsFromLS));
+}
+
+store.subscribe((mutation, state) => {
+  if (mutation.type === "setLocation") {
+    window.localStorage.setItem(
+      "weather-location",
+      JSON.stringify(state.location)
+    );
+  }
+  if (mutation.type === "setPreferences") {
+    window.localStorage.setItem(
+      "weather-preferences",
+      JSON.stringify(state.preferences)
+    );
+  }
+});
+
 app.mount("#app");
